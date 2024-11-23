e006e8e208d2fb676d032ebee139e1e2b5090d54
SANEH69 <sanehthandoh02@gmail.com>
2024-11-23 23:08:07 +0200

    Modified my time so the minutes will have two digits when less than ten

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function showDate(event) {
     if (event.target.value.length > 0) {
-        let currentDate = moment().tz(event.target.value).format("dddd, MMMM Do, YYYY h:m A");
+        let currentDate = moment().tz(event.target.value).format("dddd, MMMM Do, YYYY h:mm A");
         alert(`It is ${currentDate} in ${event.target.value}`);
     }
 }
