9e9605e7548c192f0a328be4170d2340e234fc38
SANEH69 <sanehthandoh02@gmail.com>
2024-11-25 21:19:30 +0200

    Added new elements in my JavaScript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+let name = prompt("What is your name?");
+let username = document.querySelector("#name");
+username.innerHTML = `Hello, ${name}...`;
+
 function showDate(event) {
     if (event.target.value.length > 0) {
-        let currentDate = moment().tz(event.target.value).format("dddd, MMMM Do, YYYY h:mm A");
-        alert(`It is ${currentDate} in ${event.target.value}`);
+        let currentDate = moment().tz(event.target.value).format("dddd, MMMM Do, YYYY");
+        let currentTime = moment(). tz(event.target.value).format("h:mm:ss A")
+        let cityElement = document.querySelector("#city");
+        let dateElement = document.querySelector("#date");
+        let timeElement = document.querySelector("#time");
+        cityElement.innerHTML = `${event.target.value}`;
+        dateElement.innerHTML = `${currentDate}`;
+        timeElement.innerHTML = `${currentTime}`;
     }
 }
 
