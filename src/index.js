83977b159190ce331c8dbd6d4d97ff2834777404
SANEH69 <sanehthandoh02@gmail.com>
2024-11-25 21:49:05 +0200

    Added a local time zone in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 let name = prompt("What is your name?");
+let localTimeZone = moment.tz.guess();
 let username = document.querySelector("#name");
-username.innerHTML = `Hello, ${name}...`;
+username.innerHTML = `Hello, ${name}. Your local timezone is ${localTimeZone} and the current time is ${moment().format("h:mm:ss A")}.`;
 
 function showDate(event) {
     if (event.target.value.length > 0) {
         let currentDate = moment().tz(event.target.value).format("dddd, MMMM Do, YYYY");
-        let currentTime = moment(). tz(event.target.value).format("h:mm:ss A")
+        let currentTime = moment(). tz(event.target.value).format("h:mm:ss A");
         let cityElement = document.querySelector("#city");
         let dateElement = document.querySelector("#date");
         let timeElement = document.querySelector("#time");
