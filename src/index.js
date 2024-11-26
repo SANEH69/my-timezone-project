d0c01e9729caefda34382253cbe33b94e56a3ea5
SANEH69 <sanehthandoh02@gmail.com>
2024-11-26 21:11:50 +0200

    Modified the city element to target the class instead of the id

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ username.innerHTML = `Hello, ${name}.`;
 function showDate(event) {
     if (event.target.value.length > 0) {
         let currentDate = moment().tz(event.target.value).format("dddd, MMMM Do, YYYY");
-        let currentTime = moment(). tz(event.target.value).format("h:mm:ss A");
-        let cityElement = document.querySelector("#city");
+        let currentTime = moment(). tz(event.target.value).format("hh:mm:ss A");
+        let cityElement = document.querySelector(".city");
         let dateElement = document.querySelector("#date");
         let timeElement = document.querySelector("#time");
         cityElement.innerHTML = `${event.target.value}`;
